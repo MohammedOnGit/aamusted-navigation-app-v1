3f19f11a51b865fb6ec91f18c39d6eeb18ab34c6
Aggressive-Mohammed <ibrahimmohammevis@gmail.com>
2023-10-26 19:39:30 -0700

    pin search completed

--- src/map.js
+++ src/map.js
@@ -31,4 +31,23 @@ const map = L.map("map", {
   
   // Create a layer control
   const layerControl = L.control.layers(baseMaps).addTo(map);
+   // Dummy markers
+   L.marker([51.505, -0.09], { title: 'Marker 1' }).addTo(map);
+   L.marker([51.51, -0.1], { title: 'Marker 2' }).addTo(map);
+   L.marker([51.515, -0.09], { title: 'Marker 3' }).addTo(map);
+
+
+   // PinSearch component
+   var searchBar = L.control.pinSearch({
+       position: 'topright',
+       placeholder: 'Search...',
+       buttonText: 'Search',
+       onSearch: function(query) {
+           console.log('Search query:', query);
+           // Handle the search query here
+       },
+       searchBarWidth: '200px',
+       searchBarHeight: '30px',
+       maxSearchResults: 3
+   }).addTo(map);
   
