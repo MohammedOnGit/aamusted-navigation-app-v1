bba6ea08d3d927297a3d40c408e5e93fde18e9cd
Ibrahim Mohammed <ibrahimmohammedvis@gmail.com>
2024-06-24 12:06:23 -0700

    add new campus coodenates

--- src/js/map.js
+++ src/js/map.js
@@ -170,16 +170,20 @@ const layerControl = L.control.layers(baseMaps).addTo(map);
 
 // Social medial links
 const twitterLink =
-  '<div class="child child-1"><button class="button btn-1"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="#1e90ff"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg></button></div>';
+  '<div class="child child-1"><a href="https://x.com/aamusted_gh" target="_blank" class="button btn-1"><button class="button btn-1"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="#1e90ff"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg></button></a></div>';
 
-const instagramLink =
-  '<div class="child child-2"><button class="button btn-2"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" fill="#ff00ff"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg></button></div>';
+
+
+  const instagramLink =
+  '<div class="child child-2"><a href="https://www.instagram.com/aamusted_gh?igsh=MWhzemE0aW5hcGUzOQ==" target="_blank" class="button btn-2"><button class="button btn-2"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" fill="#ff00ff"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg></button></a></div>';
+
 
 const githubLink =
   '<div class="child child-3"><button  class="button btn-3"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg></button></div>';
 
-const facbookLink =
-  '<div class="child child-4"> <button class="button btn-4"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512" fill="#4267B2"><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg></button></div>';
+  const facbookLink =
+  '<div class="child child-4"><a href="https://www.facebook.com/Aamustedgh" target="_blank" class="button btn-4"><button class="button btn-4"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512" fill="#4267B2"><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg></button></a></div>';
+
 
 //social media links all
 //writing this code is was biggest archievement througout the project
@@ -253,12 +257,150 @@ const atwimahallPopupTxt =
 const stWilliamsChaplaincyPopupTxt =
 'St Williams Chaplaincy<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#st-williams-chaplaincy-mmodal">View</button>';
 
+//Autonomy anex popUpTxt
+const autonomyAnexPopupTxt = 'Autonomy hall anex <button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#autonomy-anex">View</button>';
+
+//UBS
+const UBSAnexPopupTxt = 'University Basic Scholl<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#usb">View</button>';
+
+//Ict and library block
+const ictLibraryBlockAnexPopupTxt = 'ICT & Library Block<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#ict-library">View</button>';
+
+//aamuste clinic
+const aamustedClinicPopupTxt = 'AAMUSTED Clinic<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#aamusted-clinic">View</button>';
+
+//New Lecture block
+const newLectureBlockPopupTxt = 'New lecture block (NLB)<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#new-lecture-block">View</button>';
+
+//Opoku ware tenis court
+const opokuWareTenisCourtPopupTxt = 'Opoku Ware tenis court<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#opoku-ware-tenis">View</button>';
+
+//aamusted canteen
+const aamustedCanteenPopupTxt = 'AAMUSTED canteen<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#aamuste-canteen">View</button>';
+
+//new foculty
+const newFocultyBuildingPopupTxt = 'New foculty building<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#new-foculty-building">View</button>';
+
+//campus security post
+const campusSecurityPostPopupTxt = 'Campus security post<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#campus-security">View</button>';
+
+//TL block
+const TLblockPostPopupTxt = 'TL block<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#tl-block">View</button>';
+
+//Old library
+const oldLibraryPopupTxt = 'Old library<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#old-library">View</button>';
+
+//AAMUSTED workshop
+const aamustedWorkShopPopupTxt = 'AAMUSED Workshop<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#aamusted-workshop">View</button>';
+
+const aamustedFootballPitchPopupTxt='AAMUSED football pitch<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#aamusted-football-pitch">View</button>';
+
+const kwamanmnaRuralBankPopupTxt = 'Kwamanman rural bank<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#kwamanman-rural-bank">View</button>';
+
+const srcSecretarietPopupTxt = 'SRC secretariet<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#src-secretariet">View</button>';
+
+const aamustedMainEntrancePopupTxt = 'AAMUSTED main entrance<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#main-entrance">View</button>';
+
+
+const automotiveWorkshopPopupTxt = 'Automotive workshop<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#automotive-workshop">View</button>';
+
+const frankyJayPopupTxt = 'Franky jay<button type="button" class="btn btn-link" data-bs-toggle="modal"  data-bs-target="#franky-jay">View</button>';
+
+const ubaPopupTxt = 'UBA<button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#uba">View</button>';
+
+const constructionLabPopupTxt = 'Construction lab/workshop<button type="button" class="btn btn-link" data-bs-toggle="modal" data-bs-target="#construction-lab">View</button>';
+
 const coordinatesData = [
+  {
+    coordinates: [6.70004,-1.67906],
+    title: "Construction lab/workshop",
+    buttonId: "construction-lab",
+    popupText: constructionLabPopupTxt,
+  },
+  {
+    coordinates: [6.69736,-1.68338],
+    title: "UBA",
+    buttonId: "uba",
+    popupText: ubaPopupTxt,
+  },
+  {
+    coordinates: [6.69829,-1.68247],
+    title: "Franky Jay",
+    buttonId: "franky-jay",
+    popupText: frankyJayPopupTxt,
+  },
+  {
+    coordinates: [6.70002,-1.67947],
+    title: "Automotive workshop",
+    buttonId: "automotive-workshop",
+    popupText: automotiveWorkshopPopupTxt,
+  },
+  {
+    coordinates: [6.69585,-1.67741],
+    title: "AAMUSTED main entrance",
+    buttonId: "main-entrance",
+    popupText: aamustedMainEntrancePopupTxt,
+  },
+  {
+    coordinates: [6.69727,-1.68368],
+    title: "SRC secretariet",
+    buttonId: "src-secretariet",
+    popupText: srcSecretarietPopupTxt,
+  },
+  {
+    coordinates: [6.69717,-1.68371],
+    title: "Kwamanman rural bank",
+    buttonId: "kwamanman-rural-bank",
+    popupText: kwamanmnaRuralBankPopupTxt,
+  },
+  {
+    coordinates: [6.70113,-1.67836],
+    title: "AAMUSTED football pitch",
+    buttonId: "aamusted-football-pitch",
+    popupText: aamustedFootballPitchPopupTxt,
+  },
+  {
+    coordinates: [6.69752,-1.68093],
+    title: "AAMUSTED workshop",
+    buttonId: "aamusted-workshop",
+    popupText: aamustedWorkShopPopupTxt,
+  },
+
+  {
+    coordinates: [6.69753,-1.68172],
+    title: "Old library",
+    buttonId: "old-library",
+    popupText: oldLibraryPopupTxt,
+  },
+  {
+    coordinates: [6.69687,-1.68141],
+    title: "TL block",
+    buttonId: "TL block",
+    popupText: TLblockPostPopupTxt,
+  },
+  {
+    coordinates: [6.69676,-1.68220],
+    title: "Campus security post",
+    buttonId: "capus-security-post-btn",
+    popupText: campusSecurityPostPopupTxt,
+  },
+  {
+    coordinates: [6.69755,-1.68333],
+    title: "Opoku Ware tenis court",
+    buttonId: "opoku-ware-tenis-btn",
+    popupText: opokuWareTenisCourtPopupTxt,
+  },
   {
     coordinates: [6.69782, -1.68286],
     title: "Opoku ware hall",
     buttonId: "opoku-ware-btn",
     popupText: opokuWarePopupText,
+  },
+  {
+    coordinates: [6.69714,-1.67980],
+    title: "AAMUSTED Clinic",
+    buttonId: "aamusted-clinic-btn",
+    popupText: aamustedClinicPopupTxt,
   },
   {
     coordinates: [6.70049, -1.6823],
@@ -274,9 +416,9 @@ const coordinatesData = [
   },
   {
     coordinates: [6.69847, -1.68345],
-    title: "Canteen",
+    title: "AAMUSTED canteen",
     buttonId: "canteen-btn",
-    popupText: "Canteen",
+    popupText: aamustedCanteenPopupTxt,
   },
   {
     coordinates: [6.69841, -1.68094],
@@ -304,16 +446,16 @@ const coordinatesData = [
   },
 
   {
-    coordinates: [6.70119, -1.68301],
+    coordinates: [6.70111,-1.68312],
     title: "NFB",
     buttonId: "NFB-btn",
-    popupText: "New foculty building",
+    popupText: newFocultyBuildingPopupTxt,
   },
   {
-    coordinates: [6.7021, -1.68254],
+    coordinates: [6.70119, -1.68301],
     title: "NLB",
     buttonId: "NLB-btn",
-    popupText: "New library building",
+    popupText: newLectureBlockPopupTxt,
   },
   {
     coordinates: [6.69777, -1.68092],
@@ -341,6 +483,25 @@ const coordinatesData = [
     popupText: stWilliamsChaplaincyPopupTxt,
   },
 
+  {
+    coordinates: [6.70131,-1.67670],
+    title: "Autonomy hall anex",
+    buttonId: "autonomy-anex-btn",
+    popupText: autonomyAnexPopupTxt,
+  },
+  {
+    coordinates: [6.69678,-1.67712],
+    title: "University Basic School (UBS)",
+    buttonId: "ubs-btn",
+    popupText: UBSAnexPopupTxt,
+  },
+
+  {
+    coordinates: [6.70025,-1.68137],
+    title: "ICT & Library Block",
+    buttonId: "ict-library-btn",
+    popupText: ictLibraryBlockAnexPopupTxt,
+  },
 
   // Add more coordinate sets and button IDs as needed 6.70217,-1.68257
 ];
@@ -375,22 +536,31 @@ function setupMarkerAndFly(coordinates, buttonId, speed, popupText) {
 
 const searchMarkers = []; // Initialize an empty array to store search markers
 
-coordinatesData.forEach(function (data) {
-  setupMarkerAndFly(
-    data.coordinates,
-    data.buttonId,
-    0.5,
-    data.popupText,
-    searchMarkers
-  );
-});
+// coordinatesData.forEach(function (data) {
+//   setupMarkerAndFly(
+//     data.coordinates,
+//     data.buttonId,
+//     0.5,
+//     data.popupText,
+//     searchMarkers
+//   );
+// });
 
+//chat Bug fix
 coordinatesData.forEach(function (data) {
   const marker = L.marker(data.coordinates, { title: data.title }).addTo(map);
   const popup = L.popup({ closeButton: true }).setContent(data.popupText);
   marker.bindPopup(popup);
-  searchMarkers.push(marker);
+  searchMarkers.push(marker); // Make sure each marker is added to searchMarkers
 });
+
+
+// coordinatesData.forEach(function (data) {
+//   const marker = L.marker(data.coordinates, { title: data.title }).addTo(map);
+//   const popup = L.popup({ closeButton: true }).setContent(data.popupText);
+//   marker.bindPopup(popup);
+//   searchMarkers.push(marker);
+// });
 
 // Create the search control
 const searchBar = L.control
@@ -419,7 +589,7 @@ const searchBar = L.control
         // Fly to the marker and open its popup
         map.flyTo(markerLatLng, 19, {
           maxZoom: 19,
-          duration: 0.5,
+          duration: 0.3,
           animate: true,
         });
 
