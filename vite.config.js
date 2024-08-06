6c474eabf2b7c2f6d6599dbf5701c8fde43fd98d
ibrahimmohammedvis@gmail.com <ibrahimmohammedvis@gmail.com>
2024-08-06 21:10:40 +0000

    vite base url setup

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/aamusted-navigation-app-v1/', // Base path for assets
   build: {
     outDir: 'dist', // Output directory for build
     rollupOptions: {
